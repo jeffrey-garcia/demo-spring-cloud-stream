819f41ba1ef392d1428deff354df7bd847301a59
Jeffrey Garcia Wong <jeffrey_garcia_wong@manulife.com>
2019-10-21 16:18:30 +0800

    clean-up un-used code

--- src/main/java/com/example/demo/bindings/DemoSubscriber.java
+++ src/main/java/com/example/demo/bindings/DemoSubscriber.java
@@ -1,19 +1,19 @@
-//package com.example.demo.bindings;
-//
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
-//import org.springframework.cloud.stream.annotation.EnableBinding;
-//import org.springframework.cloud.stream.annotation.StreamListener;
-//import org.springframework.cloud.stream.messaging.Processor;
-//import org.springframework.cloud.stream.messaging.Sink;
-//
-//@EnableBinding(Processor.class)
-//public class DemoSubscriber {
-//    private static final Logger LOGGER = LoggerFactory.getLogger(DemoSubscriber.class);
-//
-//    @StreamListener(Sink.INPUT)
-//    public void listen(String payload) {
-//        LOGGER.debug("payload: {}", payload);
-//    }
-//
-//}
+package com.example.demo.bindings;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Processor;
+import org.springframework.cloud.stream.messaging.Sink;
+
+@EnableBinding(Processor.class)
+public class DemoSubscriber {
+    private static final Logger LOGGER = LoggerFactory.getLogger(DemoSubscriber.class);
+
+    @StreamListener(Sink.INPUT)
+    public void listen(String payload) {
+        LOGGER.debug("message received from queue-1, payload: {}", payload);
+    }
+
+}
--- src/main/java/com/example/demo/config/RabbitConfig.java
+++ src/main/java/com/example/demo/config/RabbitConfig.java
@@ -17,69 +17,10 @@ import org.springframework.messaging.Message;
 public class RabbitConfig {
     private static final Logger LOGGER = LoggerFactory.getLogger(RabbitConfig.class);
 
-//    @Autowired
-//    @Qualifier("tracing")
-//    Tracing tracing;
-//
-//    @Autowired
-//    @Qualifier("tracer")
-//    Tracer tracer;
-
     @RabbitListener(queues = "demo-exchange.demo-queue-2")
     public void onMessage(Message message, Channel channel) {
-//        Span span = tracer.currentSpan().name("demo/onMessage").kind(SERVER);
-//        span.remoteServiceName("rabbitmq");
-//        span.remoteIpAndPort("127.0.0.1", 5672);
-
         // handle the consuming of message
-        LOGGER.debug("received message: {}", message);
-
-//        Map<String, Object> msgHeaders = message.getHeaders();
-//        Object headersObj = msgHeaders.get("zipkin.brave.tracing.headers");
-//        Map<String, String> strHeaders = headersObj == null? null:(HashMap<String, String>)headersObj;
-//        TraceContextOrSamplingFlags result = tracing.propagation().extractor(Map<String, String>::get).extract(strHeaders);
-//
-//        if(result.context() != null){
-//            Span currentSpan = tracer.joinSpan(result.context());
-//            LOGGER.debug("trace id: {}", currentSpan.context().traceIdString());
-//            LOGGER.debug("span id: {}", currentSpan.context().spanIdString());
-//
-//            try (Tracer.SpanInScope spanInScope = tracer.withSpanInScope(currentSpan)) {
-//                currentSpan.name("onMessage").kind(Span.Kind.SERVER).remoteServiceName("demo-service").start();
-//
-//                // handle the consuming of message
-//                LOGGER.debug("received message: {}", message);
-//
-//            } catch (Exception | Error e) {
-//                // Unless you handle exceptions, you might not know the operation failed!
-//                currentSpan.error(e);
-//                throw e;
-//
-//            } finally {
-//                // note the scope is independent of the span. Always finish a span.
-//                currentSpan.finish();
-//            }
-//        } else {
-//            // handle the consuming of message
-//            LOGGER.debug("received message: {}", message);
-//        }
-
-//        Span currentSpan = tracer.currentSpan();
-//        try (Tracer.SpanInScope spanInScope = tracer.withSpanInScope(tracer.currentSpan())) {
-//            currentSpan.name("onMessage").kind(Span.Kind.CONSUMER).remoteServiceName("demo");
-//
-//            // handle the consuming of message
-//            LOGGER.debug("received message: {}", message);
-//
-//        } catch (Exception | Error e) {
-//            // Unless you handle exceptions, you might not know the operation failed!
-//            currentSpan.error(e);
-//            throw e;
-//
-//        } finally {
-//            // note the scope is independent of the span. Always finish a span.
-//            currentSpan.flush();
-//        }
+        LOGGER.debug("received message from queue-2: {}", message);
     }
 
     @Bean
