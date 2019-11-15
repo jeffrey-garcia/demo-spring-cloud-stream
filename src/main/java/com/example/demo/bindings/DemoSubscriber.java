f10b5d80bedd11583cc65a86dba177d2650817c2
Jeffrey Garcia Wong <jeffrey_garcia_wong@manulife.com>
2019-11-15 22:02:05 +0800

    code tidy up

--- src/main/java/com/example/demo/bindings/DemoSubscriber.java
+++ src/main/java/com/example/demo/bindings/DemoSubscriber.java
@@ -1,12 +1,7 @@
 package com.example.demo.bindings;
 
-import com.example.demo.channel.ErrorSink;
-import com.rabbitmq.client.Channel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.amqp.AmqpRejectAndDontRequeueException;
-import org.springframework.amqp.support.AmqpHeaders;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.cloud.stream.messaging.Sink;
@@ -17,13 +12,13 @@ import org.springframework.messaging.support.ErrorMessage;
 
 import java.util.Map;
 
-@EnableBinding({Sink.class, ErrorSink.class})
+@EnableBinding({Sink.class})
 public class DemoSubscriber {
     private static final Logger LOGGER = LoggerFactory.getLogger(DemoSubscriber.class);
 
-    /**
-     * If DLQ is not configured AND acknowledgement mode is AUTO
-     */
+//    /**
+//     * If DLQ is not configured AND acknowledgement mode is AUTO
+//     */
 //    @StreamListener(Sink.INPUT)
 //    public void listen(
 //            @Payload String messageString
@@ -34,7 +29,8 @@ public class DemoSubscriber {
 //            // simulate I/O latency in the processing of message
 //            Thread.sleep(1000);
 //
-//            throw new Exception("dummy exception!");
+//            // simulate any un-expected exception thrown in the business logic
+//            //throw new Exception("dummy exception!");
 //
 //        } catch (Exception e) {
 //            /**
@@ -48,9 +44,9 @@ public class DemoSubscriber {
 //        }
 //    }
 
-    /**
-     * If DLQ is not configured AND acknowledgement mode is MANUAL
-     */
+//    /**
+//     * If DLQ is not configured AND acknowledgement mode is MANUAL
+//     */
 //    @StreamListener(Sink.INPUT)
 //    public void listen(
 //            @Payload String messageString,
@@ -67,18 +63,19 @@ public class DemoSubscriber {
 //            // if the system crashed before the positive acknowledge,
 //            // the message will be re-queued and won't be lost
 //
-//            throw new Exception("dummy exception!");
+//            // simulate any un-expected exception thrown in the business logic
+//            //throw new Exception("dummy exception!");
 //
 //            // positive acknowledge to instruct RabbitMQ to record a message as delivered and can be discarded.
-////            channel.basicAck(deliveryTag, false);
-////            LOGGER.debug("finish processing message tag: {}, proceed to acknowledge", deliveryTag);
+//            channel.basicAck(deliveryTag, false);
+//            LOGGER.debug("finish processing message tag: {}, proceed to acknowledge", deliveryTag);
 //
 //        } catch (Exception e) {
 //            /**
 //             * purposely reject the message and don't re-queue it
 //             * - if a DLQ is configured the rejected message will be routed to the DLQ
 //             */
-////            channel.basicNack(deliveryTag, false, false);
+//            channel.basicNack(deliveryTag, false, false);
 //
 //            /**
 //             * purposely reject the message and re-queue it
@@ -86,8 +83,7 @@ public class DemoSubscriber {
 //             * infinite retry regardless of the max-attempt count
 //             * - consider to apply it for transient problem only
 //             */
-////            channel.basicNack(deliveryTag, false, true);
-//
+//            channel.basicNack(deliveryTag, false, true);
 //            throw e;
 //        }
 //    }
@@ -109,7 +105,8 @@ public class DemoSubscriber {
             // if the system crashed before the positive acknowledge,
             // the message will be re-queued and won't be lost
 
-            throw new Exception("dummy exception!");
+            // simulate any un-expected exception thrown in the business logic
+            // throw new Exception("dummy exception!");
 
         } catch (Exception e) {
             /**
@@ -122,9 +119,9 @@ public class DemoSubscriber {
     }
 
 
-    /**
-     * If DLQ is configured AND acknowledgement mode is MANUAL
-     */
+//    /**
+//     * If DLQ is configured AND acknowledgement mode is MANUAL
+//     */
 //    @StreamListener(Sink.INPUT)
 //    public void listen(
 //            @Payload String messageString,
@@ -141,11 +138,12 @@ public class DemoSubscriber {
 //            // if the system crashed before the positive acknowledge,
 //            // the message will be re-queued and won't be lost
 //
-//            throw new Exception("dummy exception!");
+//            // simulate any un-expected exception thrown in the business logic
+//            // throw new Exception("dummy exception!");
 //
 //            // positive acknowledge to instruct RabbitMQ to record a message as delivered and can be discarded.
-////            channel.basicAck(deliveryTag, false);
-////            LOGGER.debug("finish processing message tag: {}, proceed to acknowledge", deliveryTag);
+//            channel.basicAck(deliveryTag, false);
+//            LOGGER.debug("finish processing message tag: {}, proceed to acknowledge", deliveryTag);
 //
 //        } catch (Exception e) {
 //            /**
@@ -157,40 +155,22 @@ public class DemoSubscriber {
 //            /**
 //             * force a message to be dead-lettered
 //             * - throwing other exception will cause the message to be immediately re-queued
-//             *  (infinite retry without routed to DLQ)
+//             *  (infinite retry without routed to DLQ) if requeueRejected is false
 //             * - only work when consumer acknowledgement is not required, i.e. AUTO, if the
 //             * acknowledgement mode is manual, the message will stay in un-ack state and the
 //             * consumer will be blocked
 //             */
 //            throw new AmqpRejectAndDontRequeueException(e.getMessage(), e);
-////            throw e;
 //        }
 //    }
 
     @ServiceActivator(inputChannel = "errorChannel")
     public void onError(ErrorMessage errorMessage) {
         /**
-         * Intercept messages sent to the errorChannel
+         * Global error messages interceptor
          */
         // capture any delivery error
         LOGGER.debug("onError: headers:{}, payload:{}", errorMessage.getHeaders(), errorMessage.getPayload().getMessage());
     }
-
-//    @ServiceActivator(inputChannel = "errorChannel", outputChannel = "error")
-//    public ErrorMessage onError(ErrorMessage errorMessage) {
-//        /**
-//         * Intercept messages sent to the errorChannel
-//         */
-//        // capture any delivery error
-//        LOGGER.debug("onError: headers:{}, payload:{}", errorMessage.getHeaders(), errorMessage.getPayload().getMessage());
-//
-//        // consume these exceptions and handle it with custom spring integration flow
-//        return errorMessage;
-//    }
-
-//    @StreamListener("errorChannel")
-//    public void error(Message<String> message) {
-//        LOGGER.debug(" Handling ERROR: {}", message);
-//    }
 
 }
